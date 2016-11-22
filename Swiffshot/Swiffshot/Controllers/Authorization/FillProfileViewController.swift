5317b460e6caa974dda4ead73b08c33acc200d4f
Dmitry Kuklin <kuklin@sports.ru>
2016-11-23 01:14:37 +0300

    3 screens of authorisation fill users data

--- Swiffshot/Swiffshot/Controllers/Authorization/FillProfileViewController.swift
+++ Swiffshot/Swiffshot/Controllers/Authorization/FillProfileViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class FillProfileViewController: AuthorizationViewController {
+class FillProfileViewController: AuthorizationViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var avatarBtn: UIButton!
     @IBOutlet weak var nameTxt: UITextField!
     @IBOutlet weak var nickNameTxt: UITextField!
     @IBOutlet weak var startBtn: UIButton!
     
+    private let imagePicker = UIImagePickerController()
+    private var imageUrl: NSURL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         nameTxt.delegate = self
         nickNameTxt.delegate = self
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.modalPresentationStyle = .overFullScreen
+        imagePicker.navigationBar.isTranslucent = false
     }
     
     override func viewDidLayoutSubviews() {
@@ -36,9 +46,37 @@ class FillProfileViewController: AuthorizationViewController {
     }
     
     @IBAction func avatarBtnPressed(_ sender: AnyObject) {
+        present(imagePicker, animated: true, completion: nil)
+        let when = DispatchTime.now() + 0.1
+        DispatchQueue.main.asyncAfter(deadline: when) { () -> Void in
+            self.imagePicker.topViewController?.navigationItem.backBarButtonItem = UIBarButtonItem(title: " ", style: .plain, target: nil, action: nil)
+        }
     }
     
     @IBAction func startBtnPressed(_ sender: AnyObject) {
+        Defaults.sharedDefaults.userName = nameTxt.text!
+        Defaults.sharedDefaults.userNick = nickNameTxt.text!
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        avatarBtn.setImage(image, for: .normal)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageUrl = info[UIImagePickerControllerReferenceURL] as? NSURL
+        avatarBtn.setImage(info[UIImagePickerControllerOriginalImage] as? UIImage, for: .normal)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func navigationController(navigationController: UINavigationController, willShowViewController viewController: UIViewController, animated: Bool) {
+        
+        let backItem = UIBarButtonItem()
+        backItem.title = ""
+        navigationController.navigationItem.backBarButtonItem = backItem
+        navigationController.navigationBar.isTranslucent = false
     }
     
 }
---
+++ Swiffshot/Swiffshot/Utilities/Defaults.swift
@@ -0,0 +1,73 @@
+//
+//  Defaults.swift
+//  kixx
+//
+//  Created by Vladimir Borodko on 12/02/16.
+//  Copyright © 2016 kixx.today. All rights reserved.
+//
+
+import UIKit
+
+
+class Defaults {
+    static let sharedDefaults = Defaults(defaults: UserDefaults.standard)
+    
+    private var defaults: UserDefaults
+    private var token: NSObjectProtocol
+    private enum Keys: String {
+        case UserName
+        case UserNick
+        case UserAvatar
+    }
+    
+    init(defaults: UserDefaults) {
+        self.defaults = defaults
+        self.token = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIApplicationDidEnterBackground, object: nil, queue: nil) { _ in
+            defaults.synchronize()
+        }
+        defaults.register(defaults: [
+            Keys.UserName.rawValue: "",
+            Keys.UserNick.rawValue: "",
+            Keys.UserAvatar.rawValue: "",
+        ])
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(token)
+    }
+    
+    var userName: String {
+        get {
+            return defaults.string(forKey: Keys.UserName.rawValue) ?? ""
+        }
+        set {
+            defaults.set(newValue, forKey: Keys.UserName.rawValue)
+            defaults.synchronize()
+        }
+    }
+    
+    var userNick: String {
+        get {
+            return defaults.string(forKey: Keys.UserNick.rawValue) ?? ""
+        }
+        set {
+            defaults.set(newValue, forKey: Keys.UserNick.rawValue)
+            defaults.synchronize()
+        }
+    }
+    
+    var userAvatar: String {
+        get {
+            return defaults.string(forKey: Keys.UserAvatar.rawValue) ?? ""
+        }
+        set {
+            defaults.set(newValue, forKey: Keys.UserAvatar.rawValue)
+            defaults.synchronize()
+        }
+    }
+    
+    func reset() {
+        defaults.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        defaults.synchronize()
+    }
+}
